3fd00b2c85406bbaded9fe128d6aa86bad4b5931
Beau Goldberg <beaug1999@hotmail.com>
2024-06-05 22:00:58 -0600

    Update LatestPodcasts.tsx

--- components/LatestPodcasts.tsx
+++ components/LatestPodcasts.tsx
@@ -27,69 +27,77 @@ const LatestPodcasts = () => {
   };
 
   return (
-    <div className="flex flex-col gap-3 w-full h-96">
-      <h1 className="text-20 font-bold text-white-1">Latest Podcasts</h1>
+    <div className="flex flex-col w-full h-96">
+      <h1 className="text-20 font-bold text-white-1 mb-3">Latest Podcasts</h1>
       {latestPodcasts?.map((podcast, index) => (
         <div
           key={podcast._id}
-          className="flex h-20 items-center justify-between gap-6 border-b border-slate-800 pb-4 mt-2"
+          className="group hover:bg-orange-1/75 hover:rounded-md flex h-20 items-center justify-between border-b border-slate-800 py-4"
         >
           <div className="flex items-center text-center gap-4">
-            <div className="absolute group w-[70px]">
-              <span className="group-hover:hidden text-12 lg:text-16 font-normal text-white-1 mx-5">
-                {index + 1}
-              </span>
-              <span className="hidden group-hover:block mx-5 cursor-pointer">
+            <div className="grid grid-cols-12 gap-2">
+              <div className="flex items-center justify-center">
+                <span className="group-hover:hidden text-12 lg:text-16 font-normal text-white-1 mx-5">
+                  {index + 1}
+                </span>
                 <Image
                   src="/icons/Play.svg"
                   layout="fixed"
                   objectFit="cover"
                   objectPosition="center"
-                  width={50}
-                  height={50}
+                  width={26}
+                  height={26}
                   alt="play"
                   onClick={handlePlay.bind(podcast, { podcast } as any)}
+                  className="hidden group-hover:flex"
                 />
-              </span>
+              </div>
+              <div className="flex items-center">
+                <Image
+                  src={podcast.imageUrl!}
+                  width={60}
+                  height={60}
+                  alt="thumbnail"
+                  className="rounded-lg"
+                />
+              </div>
+              <div className="col-span-7 max-[766px]:col-span-7 min-[766px]:col-span-6 flex items-center">
+                <p className="font-semibold text-white-1 text-sm lg:text-md text-md truncate capitalize">
+                  {podcast?.podcastTitle}
+                </p>
+              </div>
+              <div className="max-md:hidden lg:col-span-1 flex items-center justify-center gap-1">
+                <Image
+                  src="/icons/headphone.svg"
+                  width={24}
+                  height={24}
+                  alt="ear"
+                />
+                <p className="text-14 font-medium text-white-1">
+                  {podcast?.views}
+                </p>
+              </div>
+              <div className="col-span-2 flex items-center justify-center">
+                <Image
+                  src="/icons/watch.svg"
+                  width={24}
+                  height={24}
+                  alt="duration"
+                />
+                <p className="text-14 font-medium text-white-1">
+                  {formatTime(podcast?.audioDuration)}
+                </p>
+              </div>
+              <div className="flex items-center">
+                <Image
+                  src="/icons/three-dots.svg"
+                  width={28}
+                  height={28}
+                  alt="nav"
+                  className="rotate-90 cursor-pointer"
+                />
+              </div>
             </div>
-            <Image
-              src={podcast.imageUrl!}
-              width={50}
-              height={50}
-              alt="thumbnail"
-              className="rounded-lg"
-            />
-
-            <p className="font-bold text-white-1 text-sm lg:text-md text-md max-w-[35vw] truncate capitalize">
-              {podcast?.podcastTitle}
-            </p>
-          </div>
-          <div className="flex items-center border-white-1 gap-2">
-            <Image
-              src="/icons/headphone.svg"
-              width={24}
-              height={24}
-              alt="ear"
-            />
-            <p className="text-14 font-medium text-white-1 w-[6vw]">
-              {podcast?.views}
-            </p>
-            <Image
-              src="/icons/watch.svg"
-              width={24}
-              height={24}
-              alt="duration"
-            />
-            <p className="text-14 font-medium text-white-1 w-[6vw]">
-              {formatTime(podcast?.audioDuration)}
-            </p>
-            <Image
-              src="/icons/three-dots.svg"
-              width={28}
-              height={28}
-              alt="nav"
-              className="rotate-90 cursor-pointer mx-5"
-            />
           </div>
         </div>
       ))}
